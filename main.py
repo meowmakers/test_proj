43242a82ab97359b43eacb7d4c66d78eeeee4978
elaman1996 <elaman.erkinbekov.kk@gmail.com>
2023-03-09 19:54:16 +0600

    tgbot

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ def hello():
 def good_bay():
     print('Hello Chyngyz')
     
-=======
 def move():
     print('я еду куда подадьше!')
 
     
 def num():
     print('введи число')
+
+def goodbey():
+    print('Hello Chyngyz')
+
+def second_code():
+    print('Я Двигаюсь и это хорошо!!')
